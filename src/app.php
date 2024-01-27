8b0ab336a247467c2c91beff3d6b0f7119e5075f
Sebastian Costiug <sebastian@overbyte.dev>
2024-01-27 20:07:49 +0200

    get_user_ip

--- src/app.php
+++ src/app.php
@@ -502,4 +502,33 @@ if (!function_exists('call_api')) {
             'code'     => $responseCode,
         ];
     }
+
+    if (!function_exists('get_client_ip')) {
+        /**
+         * get_client_ip
+         *
+         * @return string
+         */
+        function get_client_ip()
+        {
+            $ipaddress = '';
+            if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+            } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+                $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+            } elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+                $ipaddress = $_SERVER['HTTP_FORWARDED'];
+            } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+                $ipaddress = $_SERVER['REMOTE_ADDR'];
+            } else {
+                $ipaddress = 'UNKNOWN';
+            }
+
+            return $ipaddress;
+        }
+    }
 }
